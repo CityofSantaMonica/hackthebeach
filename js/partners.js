8a6141a6be7f4dd63eeda1b31ad544781cbc13bd
thekaveman <kegan.maher@gmail.com>
2015-09-03 10:03:30 -0700

    adding more team and partner data

--- js/partners.js
+++ js/partners.js
@@ -1,10 +1,10 @@
 (function ($) {
-  'use strict';
+  "use strict";
   
   $(".partners").each(function () {
-    var parent = $(this);
-    var kids = parent.children(".member");
-    while(kids.length) {
+    var parent = $(this),
+        kids = parent.children(".member");
+    while (kids.length) {
       parent.append(kids.splice(Math.floor(Math.random() * kids.length), 1)[0]);
     }
   });
